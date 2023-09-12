de74b012d70ccc3942c88b70493448696e6e36c2
Semedw <smusazade2000@gmail.com>
2023-09-12 20:43:30 +0400

    first commit

---
+++ OganiMaster/urls.py
@@ -0,0 +1,51 @@
+"""
+URL configuration for OganiMaster project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from core.urls import urlpatterns as core_urls
+from user.urls import urlpatterns as user_urls
+from django.contrib.auth import logout
+from django.contrib.auth.views import LogoutView
+from django.conf.urls.i18n import i18n_patterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # path('', include(core_urls)),
+    # path('user/', include(user_urls)),
+    path('', include('social_django.urls', namespace='social')),
+    path('i18n/', include('django.conf.urls.i18n'))
+]
+
+
+urlpatterns += i18n_patterns(
+    path('', include(core_urls)),
+    path('user/', include(user_urls))
+)
+# add media url
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += [
+        path('rosetta/', include('rosetta.urls')),
+        
+    ]
