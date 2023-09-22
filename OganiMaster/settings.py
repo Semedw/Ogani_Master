c5df506ae903b065730a14e3f2ba31590863ef4a
Semedw <smusazade2000@gmail.com>
2023-09-22 19:33:54 +0400

    swagger

--- OganiMaster/settings.py
+++ OganiMaster/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'ckeditor',
     'social_django',
     'rosetta',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg',
     
 ]
 
@@ -211,9 +212,15 @@ USE_TZ = True
 
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'JSON_EDITOR': True,
 }
--- OganiMaster/urls.py
+++ OganiMaster/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
@@ -25,6 +27,23 @@ from user.urls import urlpatterns as user_urls
 from django.contrib.auth import logout
 from django.contrib.auth.views import LogoutView
 from django.conf.urls.i18n import i18n_patterns
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,7 +51,7 @@ urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('core.api.urls')),
-    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # path('', include(core_urls)),
     # path('user/', include(user_urls)),
 ]
