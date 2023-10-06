65c75463f3a09d52ba48dfa53ece037cc2435ba5
Semedw <smusazade2000@gmail.com>
2023-10-06 10:44:58 +0400

    fix search bar andcategory filter

--- core/views.py
+++ core/views.py
@@ -28,29 +28,38 @@ def index(request):
 
 def shop(request):
 
-    shop_search_input = request.GET.get('news')
-    start_date = request.GET.get('start_date')
-    end_date = request.GET.get('end_date')
-    category_id = request.GET.get('category')
+    shop_search_input = request.GET.get('shop_search')
+    category_filter = request.GET.get('category_filter')
+    # start_date = request.GET.get('start_date')
+    # end_date = request.GET.get('end_date')
+    # category_id = request.GET.get('category')
 
     # Initial filters with Q()
     filters = Q()
 
     if shop_search_input:
-        filters &= Q(title__icontains=shop_search_input)
+        filters &= Q(name__icontains=shop_search_input)
 
-    if start_date:
-        if not end_date:
-            end_date = timezone.now().date()
-        filters &= Q(created_at__date__range=[start_date, end_date])
+    if category_filter:
+        # filtered_data = ProductCategory.objects.filter(title=category_filter)
+        filters &= Q(category=category_filter)
+    # else:
+    #     filtered_data = ProductCategory.objects.all()
 
-    if category_id:
-        filters &= Q(category=category_id)
+    # if start_date:
+    #     if not end_date:
+    #         end_date = timezone.now().date()
+    #     filters &= Q(created_at__date__range=[start_date, end_date])
+
+    # if category_id:
+    #     filters &= Q(category=category_id)
 
     # Fetch the news based on built filters
     products = Product.objects.filter(filters) if filters else Product.objects.all()
 
     product_count = products.count()
+
+    
 
     context = {
         'title' : 'Ogani Shop',
@@ -61,7 +70,9 @@ def shop(request):
         'discount_objects' : Discount_Product.objects.all(),
         'colors' : Colors.objects.all(),
         'sizes' : Size.objects.all(),
-        'search_input' : shop_search_input if shop_search_input else ''
+        'search_input' : shop_search_input if shop_search_input else '',
+        # 'filtered_data' : filtered_data,
+
     }
 
     return render(request, 'shop-grid.html', context)
